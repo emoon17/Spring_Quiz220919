045f08452688027abfe1a9b5da73d56044b62428
eunhyejung <wjddmsgo45@naver.com>
2022-12-28 01:08:04 +0900

    jsp, PostMapping으로 DB insert 과제

--- src/main/java/com/quiz/lesson04/Lesson04Quiz01Controller.java
+++ src/main/java/com/quiz/lesson04/Lesson04Quiz01Controller.java
@@ -1,12 +1,19 @@
 package com.quiz.lesson04;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.quiz.lesson04.bo.SellerBO;
 
 @RequestMapping("lesson04/quiz01")
 @Controller
 public class Lesson04Quiz01Controller {
 
+	@Autowired
+	private SellerBO sellerBO;
 	
 	//http://localhost/lesson04/quiz01/add_seller_view
 	@RequestMapping("/add_seller_view")
@@ -14,5 +21,17 @@ public class Lesson04Quiz01Controller {
 		return "lesson04/addSeller";
 	}
 	
+	@PostMapping("/add_seller")
+	public String addSeller(
+			@RequestParam("nickname") String nickname,
+			@RequestParam(value="profileImageUrl", required=false) String profileImageUrl,
+			@RequestParam("temperature") String temperature) {
+		
+		// db insert
+		sellerBO.addSeller(nickname, profileImageUrl, temperature);
+		
+		return "lesson04/afterAddSeller";
+		
+	}
 	
 }
